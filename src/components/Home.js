a33fcb95b511fc102262c83d54a2ade672555563
tomkf <thomaskeithferris@gmail.com>
2020-01-30 20:27:48 -0800

    rendering product cards

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
-
-//  fetch req. to API end point to launch lambda method to interact with dynamo 
+import Card from 'react-bootstrap/Card';
 
 const  API_INVOKE_URL = "https://stnvyur8of.execute-api.us-east-1.amazonaws.com/prod";
 
@@ -8,20 +7,38 @@ class Home extends Component {
   constructor() {
     super();
   this.state = {
-    apiCall: null
+    apiCall: null,
+    render: false
   };
 
   fetch(API_INVOKE_URL+'/products')
   .then(response => response.json())
   .then(data => {
-    console.log(data)
+    this.setState({ apiCall: JSON.parse(data.body), render: true })
   });
+}
+
+ renderProducts(productArray){
+  let products = productArray.map(product => (
+     <Card style={{ width: '18rem' }}>
+   <Card.Title>{product.make}</Card.Title>
+   <Card.Title>{product.name}</Card.Title>
+   <Card.Img variant="top" src={product.img} />
+      <Card.Body>
+  </Card.Body>
+ </Card>
+  ))
+
+ return (<div> {products} </div>)
 }
 
   render(){
     return(
       <div>
       <h1>Welcome</h1>
+      <div>
+      {this.state.render ? this.renderProducts(this.state.apiCall) : ""}
+      </div>
       </div>
     )
   }
